fa9024ce573e733d2798fdece7edb4c1e6ddc918
MoonStar <moonsung0331@gmail.com>
2023-08-21 22:50:46 +0900

    엔티티 설계 및 구현 변경에 따른 테스트케이스 수정

--- calendar-core/src/test/java/com/moon/fc/project/core/DomainCreateTest.java
+++ calendar-core/src/test/java/com/moon/fc/project/core/DomainCreateTest.java
@@ -1,33 +1,21 @@
 package com.moon.fc.project.core;
 
-import com.moon.fc.project.core.domain.Engagement;
-import com.moon.fc.project.core.domain.Event;
-import com.moon.fc.project.core.domain.RequestStatus;
-import com.moon.fc.project.core.domain.User;
+import com.moon.fc.project.core.domain.ScheduleType;
+import com.moon.fc.project.core.domain.entity.Schedule;
+import com.moon.fc.project.core.domain.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
 public class DomainCreateTest {
 
     @Test
     void eventCreate() {
-        final User writer = new User("writer", "email@email.com", "pw", LocalDate.now(), LocalDateTime.now());
-        final User attendee = new User("attendee", "email@email.com", "pw", LocalDate.now(), LocalDateTime.now());
-        final Event event = new Event(
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                "title",
-                "description",
-                writer,
-                LocalDateTime.now()
-        );
-
-        event.addEngagement(new Engagement(event, attendee, LocalDateTime.now(), RequestStatus.REQUESTED));
-
-        Assertions.assertEquals(event.getEngagements().get(0).getEvent().getWriter().getName(), "writer");
+        final User me = new User("meme", "email", "pw", LocalDate.now());
+        final Schedule taskSchedule = Schedule.task("할일", "청소하기", LocalDateTime.now(), me);
+        Assertions.assertEquals(taskSchedule.getScheduleType(), ScheduleType.TASK);
+        Assertions.assertEquals(taskSchedule.toTask().getTitle(), "할일");
     }
 }
