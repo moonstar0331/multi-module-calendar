8a81085a546eba87c6ab0948d3f3725795656b05
MoonStar <moonsung0331@gmail.com>
2023-08-22 18:51:48 +0900

    Dev 환경이 아닌 운영 환경에서는 실제 WebConfig 가 작동되도록 설정

--- calendar-api/src/main/java/com/moon/fc/project/api/config/WebConfig.java
+++ calendar-api/src/main/java/com/moon/fc/project/api/config/WebConfig.java
@@ -1,11 +1,13 @@
 package com.moon.fc.project.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Profile("!dev")
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
