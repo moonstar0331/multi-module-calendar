b94df6addb6b4f04f1af95edc85efc90e22de2bb
MoonStar <moonsung0331@gmail.com>
2023-08-22 16:01:15 +0900

    유저 생성, PW 확인 Core 서비스 로직 구현

--- calendar-core/src/main/java/com/moon/fc/project/core/domain/entity/repository/UserRepository.java
+++ calendar-core/src/main/java/com/moon/fc/project/core/domain/entity/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.moon.fc.project.core.domain.entity.repository;
 import com.moon.fc.project.core.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
