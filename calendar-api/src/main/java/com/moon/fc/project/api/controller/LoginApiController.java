91f43bcfd142d9c3eee1c3e351197f4695b3f7a4
MoonStar <moonsung0331@gmail.com>
2023-08-22 17:46:32 +0900

    Login API Controller 구현

---
+++ calendar-api/src/main/java/com/moon/fc/project/api/controller/LoginApiController.java
@@ -0,0 +1,37 @@
+package com.moon.fc.project.api.controller;
+
+import com.moon.fc.project.api.dto.LoginReq;
+import com.moon.fc.project.api.dto.SignUpReq;
+import com.moon.fc.project.api.service.LoginService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequiredArgsConstructor
+public class LoginApiController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/api/sign-up")
+    public ResponseEntity<Void> signUp(@RequestBody SignUpReq signUpReq, HttpSession session) {
+        loginService.signUp(signUpReq, session);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/login")
+    public ResponseEntity<Void> login(@RequestBody LoginReq loginReq, HttpSession session) {
+        loginService.login(loginReq, session);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/logout")
+    public ResponseEntity<Void> logout(HttpSession session) {
+        loginService.logout(session);
+        return ResponseEntity.ok().build();
+    }
+}
