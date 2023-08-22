89a8aed0f85694604a4134c64583b62588025f3c
MoonStar <moonsung0331@gmail.com>
2023-08-22 20:48:17 +0900

    Event 생성 API 컨트롤러 및 생성 Request DTO 작성

--- calendar-api/src/main/java/com/moon/fc/project/api/controller/ScheduleController.java
+++ calendar-api/src/main/java/com/moon/fc/project/api/controller/ScheduleController.java
@@ -1,7 +1,9 @@
 package com.moon.fc.project.api.controller;
 
 import com.moon.fc.project.api.dto.AuthUser;
+import com.moon.fc.project.api.dto.EventCreateReq;
 import com.moon.fc.project.api.dto.TaskCreateReq;
+import com.moon.fc.project.api.service.EventService;
 import com.moon.fc.project.api.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
 
     private final TaskService taskService;
+    private final EventService eventService;
 
     @PostMapping("/tasks")
     public ResponseEntity<Void> createTask(
             @RequestBody TaskCreateReq taskCreateReq,
             AuthUser authUser) {
         taskService.create(taskCreateReq, authUser);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<Void> createEvent(
+            @RequestBody EventCreateReq eventCreateReq,
+            AuthUser authUser) {
+        eventService.create(eventCreateReq, authUser);
         return ResponseEntity.ok().build();
     }
 }
